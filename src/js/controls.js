2230fb5d697ec8c1673ab561ac91bb8fe513b208
Jonny <jonnym22@gmail.com>
2020-09-02 08:37:33 +0100

    remove last searches for edge control

--- src/js/controls.js
+++ src/js/controls.js
@@ -2,8 +2,8 @@ import dom from './dom'
 
 const rounded = (scalar) => Math.round(scalar * 100) / 100;
 
-export const getEdgeLength = () => parseFloat(document.getElementById('edge').value);
-export const setEdgeLength = edgeLength => document.getElementById('edge').value = rounded(edgeLength);
+export const getEdgeLength = () => parseFloat(dom.control.edge.value);
+export const setEdgeLength = edgeLength => dom.control.edge.value = rounded(edgeLength);
 export const getXOffset = () => parseFloat(dom.control.xCentre.value);
 export const setXOffset = xOffset => dom.control.xCentre.value = rounded(xOffset);
 export const getYOffset = () => parseFloat(dom.control.yCentre.value);
